b9b80454990b9b553b9b6fde2ee9aa381287ca57
manoj <gandhamanojkutty@gmail.com>
2023-09-21 15:08:46 +0530

    env.TAG_NAME ==~ ".*" updated in nodejs.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -43,7 +43,7 @@ def call() {
             stage('Release Application') {
                 when {
                     expression {
-                        TAG_NAME ==~ ".*"
+                        env.TAG_NAME ==~ ".*"
                     }
                 }
                 steps {
