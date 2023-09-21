a7697b55e17aa95c11ac60159ca070f32b1bb4e6
manoj <gandhamanojkutty@gmail.com>
2023-09-21 18:27:55 +0530

    file upload to nexus in cart component

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -48,7 +48,7 @@ def call() {
                 }
                 steps {
                     sh 'env'
-                    sh 'echo Release Application'
+                    sh 'curl -v -u admin:admin123 --upload-file server.js http://172.31.28.176:8081/repository/cart/server.js'
                 }
             }
 
