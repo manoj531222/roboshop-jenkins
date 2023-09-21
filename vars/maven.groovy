6787105cea9bcce3f1ed30c2a25b305ad6ff6ce0
manoj <gandhamanojkutty@gmail.com>
2023-09-21 13:31:39 +0530

    code updated

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -24,6 +24,7 @@ def call() {
                 steps {
 //                    sh 'ls -l'
 //                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.29.204:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+                    echo 'Code Quality'
                 }
             }
 
