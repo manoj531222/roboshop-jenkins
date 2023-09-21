2f298b47eb296f5a8d3e01c218756cae62f691d1
manoj <gandhamanojkutty@gmail.com>
2023-09-21 22:05:19 +0530

    python,maven updated with relese

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -47,6 +47,20 @@ def call() {
                 }
             }
 
+            stage('Release Application') {
+                when {
+                    expression {
+                        env.TAG_NAME ==~ ".*"
+                    }
+                }
+                steps {
+                    sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -d ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+                    sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.28.176:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                }
+            }
+
 
         }
 
--- vars/python.groovy
+++ vars/python.groovy
@@ -41,6 +41,19 @@ def call() {
                 }
             }
 
+            stage('Release Application') {
+                when {
+                    expression {
+                        env.TAG_NAME ==~ ".*"
+                    }
+                }
+                steps {
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION'
+                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.28.176:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                }
+            }
+
 
         }
 
