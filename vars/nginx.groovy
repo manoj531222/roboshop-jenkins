854da6a28ec21b9e0d3ec37d4aff0b5a3f15a384
manoj <gandhamanojkutty@gmail.com>
2023-10-11 23:06:18 +0530

    push to docker updated not to nexus

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -57,7 +57,7 @@ def call() {
 //                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.28.176:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                     sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 693381842268.dkr.ecr.us-east-1.amazonaws.com'
                     sh 'docker build -t 693381842268.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
-                    sh 'docker push -t 693381842268.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+                    sh 'docker push 693381842268.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
                 }
             }
 
